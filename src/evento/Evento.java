c42c700e49078903de3171e40e8bed441cfac85b
Alejandro Amaya <a.amayaa2@uniandes.edu.co>
2025-10-01 14:20:06 -0500

    Clases Evento y Venue atributos completos con relaciones

--- src/evento/Evento.java
+++ src/evento/Evento.java
@@ -1,6 +1,12 @@
 package evento;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import evento.venue.Venue;
+import tiquete.TiqueteSimple;
+import usuario.comprador.Organizador;
+import usuario.comprador.Transaccion;
 
 public class Evento {
 	
@@ -8,5 +14,11 @@ public class Evento {
 	private LocalDate fecha;
 	private int hora;
 	private String idEvento;
+	
+	private Venue venueAsignado;
+	private Organizador organizadorAsignado;
+	private List<Transaccion> comprasRealizadas;
+	private List<TiqueteSimple> tiquetesMax;
+	
 
 }
--- src/evento/venue/Venue.java
+++ src/evento/venue/Venue.java
@@ -1,5 +1,17 @@
 package evento.venue;
 
+import java.util.List;
+
+import evento.Evento;
+
 public class Venue {
 
+	private String direccion;
+	private int maxCapacidad;
+	private List<String> restricciones;
+	private boolean estado;
+	
+	private List<Evento> eventosAsociados;
+	private List<Localidad> localidadesDiseñadas;
+	
 }
