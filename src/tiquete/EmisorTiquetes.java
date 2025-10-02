45c8973a270e95b5dba1be3ed7cc60bb71064573
SACV44 <sa.cardenasv1@uniandes.edu.co>
2025-10-01 22:21:06 -0500

    Emisor de Tiquetes (Commit 2)

---
+++ src/tiquete/EmisorTiquetes.java
@@ -0,0 +1,68 @@
+package tiquete;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import evento.Evento;
+import evento.venue.Localidad;
+
+public class EmisorTiquetes {
+	
+	private static Set<String> codigos = new HashSet<>();
+	
+	
+	public static Tiquete generarTiqueteSimple(Evento evento, Localidad localidad, double precio, LocalDateTime fecha, int hora) {
+		
+		String codigo;
+		
+    	int numero = ( int ) ( Math.random( ) * 100000000 );   
+		codigo = String.format( "%08d", numero );
+
+        
+        while( codigos.contains( codigo ) )
+        {
+        	int numeroe = ( int ) ( Math.random( ) * 100000000 );   
+			codigo = String.format( "%08d", numeroe );
+        }
+
+       
+        codigos.add(codigo);
+		
+		return new TiqueteSimple(codigo, precio, evento, localidad, fecha);
+		
+	}
+	
+	
+	
+	public static Tiquete generarTiqueteNumerado(Evento evento, Localidad localidad, double precio, LocalDateTime fecha, String idAsiento) {
+		
+		String codigo;
+		
+    	int numero = ( int ) ( Math.random( ) * 10000000 );   
+		codigo = String.format( "%07d", numero );
+
+        
+        while( codigos.contains("N" + codigo ) )
+        {
+        	int numeroe = ( int ) ( Math.random( ) * 10000000 );   
+			codigo = String.format( "%07d", numeroe );
+        }
+
+       
+        codigos.add("N" + codigo);
+		
+		return new TiqueteNumerado("N" + codigo, precio, evento, localidad, fecha, idAsiento);
+	
+	
+	
+	
+	
+	}
+	
+	
+
+}
