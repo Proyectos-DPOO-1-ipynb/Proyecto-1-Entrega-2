dcce14d986af5071a60563184f2508e27f5feb5f
SACV44 <sa.cardenasv1@uniandes.edu.co>
2025-10-02 07:39:53 -0500

    CORRECCIÓN TIQUETE COMPUESTP

--- src/tiquete/TiqueteCompuesto.java
+++ src/tiquete/TiqueteCompuesto.java
@@ -6,6 +6,24 @@ import java.util.ArrayList;
 public class TiqueteCompuesto extends Tiquete {
 	
 
+	private List<TiqueteSimple> tiquetes = new ArrayList<>();
+	
+	
+	TiqueteCompuesto(String idTiquete, double precio) {
+		super(idTiquete, precio);
+	}
+
+
+	public void addTiqueteSimple(Tiquete TiqueteSimple) {
+		
+		tiquetes.add((tiquete.TiqueteSimple) TiqueteSimple);
+		
+		
+		
+	}
+	
+	
+	
 		
 	}
 	
