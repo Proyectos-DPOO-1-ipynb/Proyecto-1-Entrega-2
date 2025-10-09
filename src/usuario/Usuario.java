7bb4884d4717976f8fb82d262bc21fecd667d630
Alejandro Amaya <a.amayaa2@uniandes.edu.co>
2025-10-09 13:43:41 -0500

    Atributos Usuario

--- src/usuario/Usuario.java
+++ src/usuario/Usuario.java
@@ -1,5 +1,16 @@
 package usuario;
 
-public class Usuario {
+import java.time.LocalDate;
 
+public class Usuario {
+	
+	private String nombre;
+	private String apellidos;
+	private LocalDate fecha;
+	private String login;
+	private String password;
+	private String correo;
+	
+	// TODO transacciones
+	
 }
