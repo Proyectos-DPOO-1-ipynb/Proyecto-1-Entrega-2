a458e113e67d4f2e2d135eae3824aeb2e5dfb306
Alejandro Amaya <a.amayaa2@uniandes.edu.co>
2025-10-12 02:51:52 -0500

    Método descuento Tiquete + Clase Dcto

--- src/evento/venue/Descuento.java
+++ src/evento/venue/Descuento.java
@@ -1,5 +1,38 @@
 package evento.venue;
 
-public class Descuento {
+import java.time.LocalDate;
 
+public class Descuento {
+	
+	private double porcentaje;
+	private LocalDate fechaInicio;
+	private LocalDate fechaFin;
+	
+	private Localidad localidadAsociada;
+
+	public Descuento(double porcentaje, LocalDate fechaInicio, LocalDate fechaFin, Localidad localidadAsociada) {
+		this.porcentaje = porcentaje;
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		this.localidadAsociada = localidadAsociada;
+	}
+
+	public double getPorcentaje() {
+		return porcentaje;
+	}
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+
+	public Localidad getLocalidadAsociada() {
+		return localidadAsociada;
+	}
+	
+	
+	
 }
--- src/tiquete/Tiquete.java
+++ src/tiquete/Tiquete.java
@@ -1,5 +1,9 @@
 package tiquete;
 
+import java.util.Optional;
+
+import evento.venue.Descuento;
+
 public abstract class Tiquete {
 	
 	private String idTiquete;
@@ -8,7 +12,23 @@ public abstract class Tiquete {
 	public Tiquete(String idTiquete, double precio){
 		
 		this.idTiquete = idTiquete;
-		this.precio = precio;
+		this.precio = precio;	
+	}
+	
+	public double calcularPrecioFinal(double cargoServicioPct, double costoEmisionFijo, Descuento dcto) {
+		double base = this.precio;
+		
+		if (dcto != null) {
+			double porcentaje = dcto.getPorcentaje();
+			
+			// El descuento se calcula sobre el precio base 
+			base = base - (porcentaje * base);
+		}
+		double cargoServicio = base * cargoServicioPct;
+		return base + cargoServicio + costoEmisionFijo;
 		
 	}
+
+	
+	
 }
