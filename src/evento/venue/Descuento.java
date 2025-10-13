ddda73b9f093f40824ce3abf3ce9c6b81303382f
SACV44 <sa.cardenasv1@uniandes.edu.co>
2025-10-12 19:05:38 -0500

    Clase Descuento

--- src/evento/venue/Descuento.java
+++ src/evento/venue/Descuento.java
@@ -1,5 +1,70 @@
 package evento.venue;
 
+import java.time.LocalDate;
+
+import evento.Evento;
+import usuario.comprador.Organizador;
+
 public class Descuento {
 
+	private Organizador organizador;
+	private Evento evento;
+	private Localidad localidad;
+	private double porcentaje;
+	private LocalDate fechaInicio;
+	private LocalDate fechaFin;
+	private String estado;
+	
+	
+	public Descuento(Organizador organizador, Evento evento, Localidad localidad, double porcentaje, LocalDate fechaInicio,
+			LocalDate fechaFin) throws Exception {
+		
+		
+		if (porcentaje <= 0) {
+			
+			throw new Exception("Se ingresó incorrectamente el descuento");
+			
+		}
+		
+		if(fechaInicio.isAfter(fechaFin)) {
+			
+			throw new Exception("Fechas mal ingresadas. Fecha de inicio es mayor a fecha de final");
+		}
+		
+		if(localidad.verificadorAsociacionEvento(evento) == false) {
+			
+			throw new Exception("La localidad ingresada no corresponde al evento ingresado");
+		}
+		
+		
+		
+		
+		this.organizador = organizador;
+		this.evento = evento;
+		this.localidad = localidad;
+		this.porcentaje = porcentaje;
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		
+		
+		
+		
+	
+		
+		
+		
+	}
+	
+	
+	
+	public double getPorcentaje() {
+		
+		return porcentaje;
+		
+	}
+	
+	
+	
+	
+	
 }
